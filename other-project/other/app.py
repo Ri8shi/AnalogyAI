85ad23b0b83809ffba727988fdc71c0f27b884fe
rishihe <nagderishi@gmail.com>
2026-06-15 08:18:28 +0530

    final change

--- other-project/other/app.py
+++ other-project/other/app.py
@@ -12,6 +12,7 @@ def configure_gemini(api_key: str) -> genai.GenerativeModel:
         ),
     )
 
+api_keey = "AQ.Ab8RN6JTVNq3JspoW3K5_EW16ZOSUO4lhvJ_5HBv6RTqIyCf9A"
 
 try:
     model = configure_gemini(api_keey)
